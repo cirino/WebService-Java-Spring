cc4adb76c541064b4e942e5ebaa5e8b64bd0b707
Dagmar Cirino <dagmar.mv@gmail.com>
2020-10-16 01:56:34 -0300

    Projeto com banco de dados h2

---
+++ src/main/java/com/casadodev/course/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.casadodev.course.repositories;
+
+import com.casadodev.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
---
+++ src/main/java/com/casadodev/course/resources/UserResourse.java
@@ -0,0 +1,30 @@
+package com.casadodev.course.resources;
+
+import com.casadodev.course.entities.User;
+import com.casadodev.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResourse {
+
+	@Autowired
+	private UserService service;
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
